bb55cd51c4364003c4b66c54910c81ed068158db
Francois PARMENTIER <parmentier.corporate@gmail.com>
2023-07-11 16:52:37 +0200

    Added src

---
+++ src/main/java/com/cultureclashpodcast/app/CultureClashPodcastApplication.java
@@ -0,0 +1,13 @@
+package com.cultureclashpodcast.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CultureClashPodcastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CultureClashPodcastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cultureclashpodcast/app/CultureClashPodcastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cultureclashpodcast.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CultureClashPodcastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
